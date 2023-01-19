86e0a5fbb1eff467dda42fdbc74282f7b8abeefa
mrcocoball <gunmaru93@gmail.com>
2023-01-19 20:17:55 +0900

    #4 - Auditing 기능 활성화

--- src/main/java/com/dateplanner/DatePlannerApplication.java
+++ src/main/java/com/dateplanner/DatePlannerApplication.java
@@ -2,8 +2,10 @@ package com.dateplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DatePlannerApplication {
 
     public static void main(String[] args) {
