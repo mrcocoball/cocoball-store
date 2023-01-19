9822acb73f691a2c106bbd18954efdf291bbc8c4
mrcocoball <gunmaru93@gmail.com>
2023-01-19 23:32:44 +0900

    #10 - Dto 추가 / 수정
    * 카테고리 장소 검색의 경우 행정동 상세 주소를 제공하지 않고 있어 Dto에서는 일단 주석 처리 하였음

--- src/main/java/com/dateplanner/api/dto/KakaoApiResponseDto.java
+++ src/main/java/com/dateplanner/api/dto/KakaoApiResponseDto.java
@@ -1,4 +1,21 @@
 package com.dateplanner.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class KakaoApiResponseDto {
+
+    @JsonProperty("meta")
+    private MetaDto metaDto;
+
+    @JsonProperty("documents")
+    private List<DocumentDto> documentList;
+
 }
