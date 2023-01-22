829cef88d7f1d47183e37d701abac596e3c3ddec
mrcocoball <gunmaru93@gmail.com>
2023-01-22 23:25:31 +0900

    #13 - 엔티티 수정

--- src/main/java/com/dateplanner/place/entity/Category.java
+++ src/main/java/com/dateplanner/place/entity/Category.java
@@ -5,19 +5,41 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Persistable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-@Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Category extends BaseEntity {
+public class Category extends BaseEntity implements Persistable<String> {
 
     @Id
+    @Column(name = "category_id")
     private String id;
 
     private String categoryName;
+
+    private Category(String id) {
+        this.id = id;
+    }
+
+    public static Category of(String id) {
+        return new Category(id);
+    }
+
+    // persistable 재정의
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreatedAt() == null;
+    }
 
 }
