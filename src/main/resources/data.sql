7ed2639431a7473283bfbd21307b04394ff0e944
mrcocoball <gunmaru93@gmail.com>
2023-01-22 23:07:33 +0900

    #13 - 카테고리 Mock Data 추가
    테스트용으로 카테고리 테이블에 필요한 Mock Data를 추가하였음

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (category_id, created_at, modified_at, category_name) values
+('MT1', now(), now(), '대형 마트'),
+('CS2', now(), now(), '편의점'),
+('PK6', now(), now(), '주차장'),
+('OL7', now(), now(), '주유소, 충전소'),
+('SW8', now(), now(), '지하철역'),
+('CT1', now(), now(), '문화시설'),
+('AT4', now(), now(), '관광명소'),
+('AD5', now(), now(), '숙박'),
+('FD6', now(), now(), '음식점'),
+('CE7', now(), now(), '카페');
