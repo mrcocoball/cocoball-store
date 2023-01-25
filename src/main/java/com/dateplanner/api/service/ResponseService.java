5d9c75c8342dab4fd7d92d8e2b21ab3b756c029e
mrcocoball <gunmaru93@gmail.com>
2023-01-25 12:47:37 +0900

    #18 - 공통 Response 및 Respose Service 구축
    * DTO나 단순 성공/실패 여부를 Response로 만들어 반환하는 서비스를 구축

---
+++ src/main/java/com/dateplanner/api/service/ResponseService.java
@@ -0,0 +1,103 @@
+package com.dateplanner.api.service;
+
+import com.dateplanner.api.model.CommonResult;
+import com.dateplanner.api.model.ListResult;
+import com.dateplanner.api.model.PageResult;
+import com.dateplanner.api.model.SingleResult;
+import com.dateplanner.api.response.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j(topic = "SERVICE")
+@Service
+public class ResponseService {
+
+    /**
+     * API가 반환한 모델을 처리, 결과 모델에 데이터를 넣어줌
+     */
+
+
+    /**
+     * 단일 데이터 결과 처리 메소드
+     */
+    public <T> SingleResult<T> getSingleResult(T data) {
+
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+
+    }
+
+    /**
+     * 복수 데이터 결과 처리 메소드
+     */
+    public <T>ListResult<T> getListResult(List<T> list) {
+
+        ListResult<T> result = new ListResult<>();
+        result.setData(list);
+        setSuccessResult(result);
+        return result;
+
+    }
+
+    /**
+     * 페이지 결과 처리 메소드
+     */
+    public <T> PageResult<T> getPageResult(Page<T> page) {
+
+        PageResult<T> result = new PageResult<>();
+        result.setData(page);
+        setSuccessResult(result);
+        return result;
+    }
+
+    /**
+     * 성공 결과만 처리 (단순히 성공 / 실패 여부 응답용)
+     */
+    public CommonResult getSuccessResult() {
+
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+
+    }
+
+    /**
+     * 실패 결과만 처리 (단순히 성공 / 실패 여부 응답용)
+     */
+    public CommonResult getFailResult() {
+
+        CommonResult result = new CommonResult();
+        setFailResult(result);
+        return result;
+
+    }
+
+
+    /**
+     * API 요청 성공 시 응답 모델을 성공 데이터로 세팅
+     */
+    private void setSuccessResult(CommonResult result) {
+
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMsg(CommonResponse.SUCCESS.getMsg());
+
+    }
+
+    /**
+     * API 요청 실패 시 응답 모델을 실패 데이터로 세팅
+     */
+    private void setFailResult(CommonResult result) {
+
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
+
+    }
+
+}
