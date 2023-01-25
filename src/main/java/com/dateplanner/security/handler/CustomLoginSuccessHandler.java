b716804ef72d0ce489a3f8025c66961473a6c035
mrcocoball <gunmaru93@gmail.com>
2023-01-25 23:56:11 +0900

    #18 - Jwt 생성기, 인증 필터 및 유저 인증 서비스 추가

---
+++ src/main/java/com/dateplanner/security/handler/CustomLoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.dateplanner.security.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j(topic = "SECURITY")
+@RequiredArgsConstructor
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        log.info("[CustomLoginSuccessHandler onAuthenticationSuccess] login success handler---------------------------");
+
+    }
+
+}
