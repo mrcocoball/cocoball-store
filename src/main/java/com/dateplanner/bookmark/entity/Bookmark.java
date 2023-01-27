8fa0742e59afd61045793d47a9c2b0e5a4e2f5e0
mrcocoball <gunmaru93@gmail.com>
2023-01-27 23:32:39 +0900

    #22 - GeneratedValue 수정

--- src/main/java/com/dateplanner/bookmark/entity/Bookmark.java
+++ src/main/java/com/dateplanner/bookmark/entity/Bookmark.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 public class Bookmark extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
