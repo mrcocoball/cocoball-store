efdebcbb4a699b069485037c5f39bee1732320d9
mrcocoball <gunmaru93@gmail.com>
2023-01-28 23:25:43 +0900

    #23 - CORS 처리 관련 SecurityConfig 수정
    * CORS 처리를 위해 SecurityConfig 내에 CorsConfigurationSource Bean을 등록하였고 filter에서 작동하도록 수정

--- src/main/java/com/dateplanner/config/SecurityConfig.java
+++ src/main/java/com/dateplanner/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Slf4j(topic = "SECURITY")
 @Configuration
@@ -33,7 +38,6 @@ public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
 
-    // TODO: 인증 기능 추후 구현
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -61,6 +65,11 @@ public class SecurityConfig {
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        // CORS
+        http.cors(httpSecurityCorsConfigurer -> {
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+        });
+
         return http
                 // 어노테이션 형식으로 변경하였음
 //                .authorizeHttpRequests(auth -> auth
@@ -85,6 +94,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // CORS 관련 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE")); // 허용할 메서드
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type")); // 허용할 Http 헤더
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
