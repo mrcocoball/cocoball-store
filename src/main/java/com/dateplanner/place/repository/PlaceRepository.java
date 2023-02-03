d436793146a57384c616d828bbc6e9979040d6e5
mrcocoball <gunmaru93@gmail.com>
2023-02-03 20:23:26 +0900

    #39 - 장소 검색 저장 중복 체크 로직 변경
    * 기존에는 API로 가져온 Document 리스트 내의 placeId들을 직접 DB에서 조회하였으나 로직을 변경하여 DB에 저장된 placeId들을 최대한 적은 쿼리로 많이 조회해여 어플리케이션 내에서 비교하도록 변경

--- src/main/java/com/dateplanner/place/repository/PlaceRepository.java
+++ src/main/java/com/dateplanner/place/repository/PlaceRepository.java
@@ -2,6 +2,8 @@ package com.dateplanner.place.repository;
 
 import com.dateplanner.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,7 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
     Optional<Place> findByPlaceId(String placeId);
 
     List<Place> findByAddressNameStartingWithAndCategory_Id(String address, String category);
+
+    @Query("select p.placeId from Place p where p.region1DepthName = :region1 and p.region2DepthName = :region2")
+    List<String> findPlaceIdByRegion1DepthNameAndRegion2DepthName(@Param("region1") String region1, @Param("region2") String region2);
 }
