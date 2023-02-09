28ecbada68b46a012138b84815cda363c288b163
mrcocoball <gunmaru93@gmail.com>
2023-02-09 23:58:48 +0900

    #30 - Response 관련 @Schema 추가

--- src/main/java/com/dateplanner/api/model/PageResult.java
+++ src/main/java/com/dateplanner/api/model/PageResult.java
@@ -1,5 +1,6 @@
 package com.dateplanner.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
@@ -12,6 +13,7 @@ public class PageResult<T> extends CommonResult {
      * API 반환값이 페이지 타입일 경우 처리
      */
 
+    @Schema(description = "페이지 형식 데이터")
     private Page<T> data;
 
 }
