e3cd5d3d054678480bf040d0bc68f0637b41159d
mrcocoball <gunmaru93@gmail.com>
2023-02-16 19:49:57 +0900

    #60 - 업데이트 기능 수정

--- src/main/java/com/dateplanner/admin/place/controller/PlaceAdminController.java
+++ src/main/java/com/dateplanner/admin/place/controller/PlaceAdminController.java
@@ -4,8 +4,8 @@ import com.dateplanner.admin.place.dto.PlaceCrawlingDto;
 import com.dateplanner.admin.place.dto.PlaceStatusDto;
 import com.dateplanner.admin.place.service.PlaceAdminService;
 import com.dateplanner.admin.place.service.PlaceCrawlingService;
-import com.dateplanner.api.model.CommonResult;
 import com.dateplanner.api.model.PageResult;
+import com.dateplanner.api.model.SingleResult;
 import com.dateplanner.api.service.ResponseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,6 +16,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j(topic = "CONTROLLER")
 @Tag(name = "8. [어드민 전용] PlaceAdminController - 장소 정보 수정 관련 API")
@@ -31,7 +33,7 @@ public class PlaceAdminController {
     // 어드민 권한 요구 추가 예정
     @Operation(summary = "[GET] 이미지 URL이 추가가 되지 않은 장소 조회",
             description = "DB에 생성이 되었으나 이미지 URL이 추가 되지 않은 장소(이미지 URL이 NULL인 장소)들을 조회합니다.")
-    @GetMapping("/api/v1/admin/places1")
+    @GetMapping("/api/v1/admin/nullPlaces")
     public PageResult<PlaceStatusDto> getImageUrlNullPlacesV1(@PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
 
         return responseService.getPageResult(placeAdminService.getImageUrlNullPlacesV1(pageable));
@@ -41,7 +43,7 @@ public class PlaceAdminController {
     // 어드민 권한 요구 추가 예정
     @Operation(summary = "[GET] 이미지 URL 업데이트를 진행하였으나 이미지가 존재하지 않은 장소 조회",
             description = "이미지 URL 업데이트를 진행하였으나 원본 장소의 이미지가 존재하지 않아 추가를 하지 못했던 장소들을 조회합니다.")
-    @GetMapping("/api/v1/admin/places2")
+    @GetMapping("/api/v1/admin/notExistPlaces")
     public PageResult<PlaceStatusDto> getImageUrlNotExistPlacesV1(@PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
 
         return responseService.getPageResult(placeAdminService.getImageUrlNotExistPlacesV1(pageable));
@@ -49,12 +51,25 @@ public class PlaceAdminController {
 
 
     // 어드민 권한 요구 추가 예정, 테스트용
-    @Operation(summary = "[POST] 크롤링 테스트(Selenium)",
-            description = "크롤링 테스트")
-    @PostMapping("/api/v1/admin/places")
+    @Operation(summary = "[POST] 장소 이미지, 설명 크롤링 요청 및 결과 확인 테스트",
+            description = "이미지 URL이 추가되지 않은 장소들을 조회한 뒤 해당 장소들에 대해 크롤링을 요청하고 크롤링 결과를 조회합니다. <br>" +
+                    "실제 DB 장소에는 저장되지는 않습니다.")
+    @PostMapping("/api/v1/admin/crawling")
     public PageResult<PlaceCrawlingDto> crawlingPlacesV1(@PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
 
         return responseService.getPageResult(placeCrawlingService.searchAndCrawling(pageable));
+    }
+
+    // 어드민 권한 요구 추가 예정, 테스트용
+    @Operation(summary = "[POST] 장소 이미지, 설명 크롤링 요청 및 결과 확인",
+            description = "이미지 URL이 추가되지 않은 장소들을 조회한 뒤 해당 장소들에 대해 크롤링을 요청하고 <br>" +
+                    "크롤링된 정보를 토대로 장소 정보를 업데이트합니다. 업데이트된 장소의 개수가 반환됩니다.")
+    @PostMapping("/api/v1/admin/crawlingAndUpdate")
+    public SingleResult<Long> crawlingAndUpdatePlacesV1() {
+
+        List<PlaceCrawlingDto> dtos = placeCrawlingService.searchAndCrawling();
+
+        return responseService.getSingleResult(placeAdminService.updatePlacesV1(dtos));
     }
 
 }
--- src/main/java/com/dateplanner/admin/place/service/PlaceAdminService.java
+++ src/main/java/com/dateplanner/admin/place/service/PlaceAdminService.java
@@ -1,5 +1,6 @@
 package com.dateplanner.admin.place.service;
 
+import com.dateplanner.admin.place.dto.PlaceCrawlingDto;
 import com.dateplanner.admin.place.dto.PlaceStatusDto;
 import com.dateplanner.api.PaginationService;
 import com.dateplanner.place.repository.PlaceRepository;
@@ -9,29 +10,65 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 @Slf4j(topic = "SERVICE")
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Service
 public class PlaceAdminService {
 
     private final PlaceRepository placeRepository;
     private final PaginationService paginationService;
+    private final static String NOT_EXIST_IMAGE = "NOT EXISTS";
 
 
+    @Transactional(readOnly = true)
     public Page<PlaceStatusDto> getImageUrlNullPlacesV1(Pageable pageable) {
 
         return paginationService.listToPage(placeRepository.findByImageUrlIsNull().stream().map(PlaceStatusDto::from).collect(Collectors.toList()), pageable);
     }
 
+
+    @Transactional(readOnly = true)
     public Page<PlaceStatusDto> getImageUrlNotExistPlacesV1(Pageable pageable) {
 
-        String condition = "크롤링 결과 이미지 없음";
+        return paginationService.listToPage(placeRepository.findByImageUrlIs(NOT_EXIST_IMAGE).stream().map(PlaceStatusDto::from).collect(Collectors.toList()), pageable);
+    }
 
-        return paginationService.listToPage(placeRepository.findByImageUrlIs(condition).stream().map(PlaceStatusDto::from).collect(Collectors.toList()), pageable);
+
+    public Long updatePlacesV1(List<PlaceCrawlingDto> dtos) {
+
+        // 저장 시간 비교 계산용 측정
+        long beforeTime = System.currentTimeMillis();
+
+        Long count = 0L;
+
+        for (PlaceCrawlingDto dto : dtos) {
+            List<String> tags = dto.getTags();
+            StringJoiner stringJoiner = new StringJoiner(", ");
+            stringJoiner.setEmptyValue("");
+            if (!ObjectUtils.isEmpty(tags) || tags != null) {
+                for (String tag : tags) {
+                    stringJoiner.add(tag);
+                }
+            }
+            String description = String.valueOf(stringJoiner);
+            if (description.equals("")) {description = null;}
+            placeRepository.updateImageUrlAndDescription(dto.getPlaceId(), dto.getImageUrl(), description);
+            count += 1L;
+            log.info("[PlaceCrawlingService searchAndCrawling] - {} of {} complete", count, dtos.size());
+        }
+
+        // 저장 시간 비교 계산용 측정
+        long afterTime = System.currentTimeMillis();
+        log.info("elapsed time : " + (afterTime - beforeTime));
+
+        return count;
     }
 
 }
