869c3c56becfd7fbcfd1e49cfabdfddd69e12ea8
mrcocoball <gunmaru93@gmail.com>
2023-02-28 23:53:04 +0900

    #76 - 공지사항 및 공지사항 카테고리 엔티티, Dto 추가

---
+++ src/main/java/com/dateplanner/admin/consumer/dto/AnnouncementModifyRequestDto.java
@@ -0,0 +1,42 @@
+package com.dateplanner.admin.consumer.dto;
+
+import com.dateplanner.admin.consumer.entity.Announcement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Slf4j(topic = "DTO")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnnouncementModifyRequestDto {
+
+    private Long id;
+
+    @NotNull(message = "내용이 입력되어야 합니다")
+    @NotEmpty(message = "내용이 입력되어야 합니다")
+    private String title;
+
+    @NotNull(message = "내용이 입력되어야 합니다")
+    @NotEmpty(message = "내용이 입력되어야 합니다")
+    private String description;
+
+    private Long categoryId;
+
+    public static AnnouncementModifyRequestDto from(AnnouncementDto dto) {
+
+        return AnnouncementModifyRequestDto.builder()
+                .id(dto.getId())
+                .title(dto.getTitle())
+                .description(dto.getDescription())
+                .categoryId(dto.getCategoryId())
+                .build();
+    }
+
+}
---
+++ src/main/java/com/dateplanner/admin/consumer/dto/AnnouncementRequestDto.java
@@ -0,0 +1,41 @@
+package com.dateplanner.admin.consumer.dto;
+
+import com.dateplanner.admin.consumer.entity.Announcement;
+import com.dateplanner.admin.consumer.entity.AnnouncementCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Slf4j(topic = "DTO")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnnouncementRequestDto {
+
+    @NotNull(message = "내용이 입력되어야 합니다")
+    @NotEmpty(message = "내용이 입력되어야 합니다")
+    private String title;
+
+    @NotNull(message = "내용이 입력되어야 합니다")
+    @NotEmpty(message = "내용이 입력되어야 합니다")
+    private String description;
+
+    private Long categoryId;
+
+    public Announcement toEntity(String title, String description, AnnouncementCategory announcementCategory) {
+
+        return Announcement.of(
+                title,
+                description,
+                announcementCategory
+        );
+
+    }
+
+}
