59bcc32dd2c578db5d80657d07210f2a42a0c516
mrcocoball <gunmaru93@gmail.com>
2023-03-06 22:56:44 +0900

    #78 - dto 수정

--- src/main/java/com/dateplanner/admin/user/dto/UserResponseDto.java
+++ src/main/java/com/dateplanner/admin/user/dto/UserResponseDto.java
@@ -1,19 +1,39 @@
 package com.dateplanner.admin.user.dto;
 
 import com.dateplanner.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
 
 @Slf4j(topic = "DTO")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserResponseDto {
 
-    private final String email;
-    private final String nickname;
+    private Long id;
+    private String email;
+    private String nickname;
+    private boolean social;
+    private boolean deleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
-    public UserResponseDto(User user) {
-        this.email = user.getEmail();
-        this.nickname = user.getNickname();
+    public static UserResponseDto from(User entity) {
+        return UserResponseDto.builder()
+                .id(entity.getUid())
+                .email(entity.getEmail())
+                .nickname(entity.getNickname())
+                .social(entity.isSocial())
+                .deleted(entity.isDeleted())
+                .createdAt(entity.getCreatedAt())
+                .modifiedAt(entity.getModifiedAt())
+                .build();
     }
 
 }
