85a46e1e2416106cc607ed479e703841cbf4e586
mrcocoball <gunmaru93@gmail.com>
2023-03-26 17:00:32 +0900

    #92 - 기존 서비스 Dto @Schema 추가

--- src/main/java/com/dateplanner/support/dto/FaqDto.java
+++ src/main/java/com/dateplanner/support/dto/FaqDto.java
@@ -1,11 +1,15 @@
 package com.dateplanner.support.dto;
 
 import com.dateplanner.admin.consumer.entity.FavoriteAnswer;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
 
 @Slf4j(topic = "DTO")
 @Getter
@@ -14,13 +18,25 @@ import lombok.extern.slf4j.Slf4j;
 @Builder
 public class FaqDto {
 
+    @Schema(description = "FAQ 질문 카테고리 내 답변 ID")
     private Long id;
 
+    @Schema(description = "FAQ 질문 카테고리 ID")
     private Long categoryId;
 
+    @Schema(description = "제목")
     private String title;
 
+    @Schema(description = "내용")
     private String description;
+
+    @Schema(description = "작성 시간")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+
+    @Schema(description = "수정 시간")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime modifiedAt;
 
     public static FaqDto from(FavoriteAnswer entity) {
         return FaqDto.builder()
@@ -28,6 +44,8 @@ public class FaqDto {
                 .categoryId(entity.getFavoriteQuestionCategory().getId())
                 .title(entity.getTitle())
                 .description(entity.getDescription())
+                .createdAt(entity.getCreatedAt())
+                .modifiedAt(entity.getModifiedAt())
                 .build();
     }
 
