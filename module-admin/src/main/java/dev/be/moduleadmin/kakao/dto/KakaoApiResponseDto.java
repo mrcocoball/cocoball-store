dbca4adb34be54cf6aac994acdc8ecf9af6c30f0
mrcocoball <gunmaru93@gmail.com>
2023-03-31 00:43:01 +0900

    #96 - 카카오 API 기능 이관

---
+++ module-admin/src/main/java/dev/be/moduleadmin/kakao/dto/KakaoApiResponseDto.java
@@ -0,0 +1,27 @@
+package dev.be.moduleadmin.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j(topic = "DTO")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    @JsonProperty("meta")
+    private MetaDto metaDto;
+
+    @JsonProperty("documents")
+    private List<DocumentDto> documentList;
+
+    public void setDocumentList(List<DocumentDto> documentList) {
+        this.documentList = documentList;
+    }
+
+}
---
+++ module-admin/src/main/java/dev/be/moduleadmin/kakao/dto/MetaDto.java
@@ -0,0 +1,24 @@
+package dev.be.moduleadmin.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "DTO")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+
+    @JsonProperty("same_name")
+    private RegionInfoDto sameName;
+
+    @JsonProperty("total_count")
+    private Integer totalCount;
+
+    @JsonProperty("pageable_count")
+    private Integer pageableCount;
+
+}
