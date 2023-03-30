25f41aeeb5e0251b95625400e596a2186c8db7e4
mrcocoball <gunmaru93@gmail.com>
2023-03-30 17:15:27 +0900

    #96 - API 관련 서비스, 기능 이관

---
+++ module-api/src/main/java/dev/be/moduleapi/api/model/CommonResult.java
@@ -0,0 +1,24 @@
+package dev.be.moduleapi.api.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonResult {
+
+    /**
+     * 전달될 데이터와 별개로 API의 처리 여부, 상태, 메시지가 담긴 데이터
+     */
+
+    @Schema(description = "응답 성공 여부, True / False")
+    private boolean success;
+
+    @Schema(description = "응답 코드, >=0 : 정상, <0 : 비정상")
+    private int code;
+
+    @Schema(description = "응답 메시지")
+    private String msg;
+
+}
---
+++ module-api/src/main/java/dev/be/moduleapi/api/response/CommonResponse.java
@@ -0,0 +1,20 @@
+package dev.be.moduleapi.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CommonResponse {
+
+    /**
+     * 공통 응답 모델을 success / fail로 처리
+     */
+
+    SUCCESS(0, "success"),
+    FAIL(-1, "fail");
+
+    private int code;
+    private String msg;
+
+}
