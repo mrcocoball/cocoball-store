0d9b321a8e41ed60a52b17372effcd8c06ee70a8
mrcocoball <gunmaru93@gmail.com>
2023-04-02 23:37:17 +0900

    #102 - Security Config 재수정
    AccessDeniedHandler를 따로 커스텀할 필요가 없어서 삭제 처리하였음. 접근 권한이 없을 경우 에러 페이지 노출

--- module-admin/src/main/java/dev/be/moduleadmin/config/SecurityConfig.java
+++ module-admin/src/main/java/dev/be/moduleadmin/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package dev.be.moduleadmin.config;
 
-import dev.be.moduleadmin.security.handler.Custom403ExceptionHandler;
 import dev.be.moduleadmin.security.handler.CustomLoginFailureHandler;
 import dev.be.moduleadmin.security.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
@@ -8,24 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Slf4j(topic = "SECURITY")
 @Configuration
@@ -45,12 +34,11 @@ public class SecurityConfig {
                 .userDetailsService(customUserDetailsService)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .antMatchers("/admin/**").permitAll()
+                        .antMatchers("/user/login").permitAll()
+                        .antMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                         .and())
                 .formLogin().loginPage("/user/login").failureHandler(authenticationFailureHandler()).and().logout().logoutSuccessUrl("/")
-                .and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
                 .and()
                 .build();
     }
@@ -63,12 +51,6 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-
-        return new Custom403ExceptionHandler();
     }
 
     @Bean
