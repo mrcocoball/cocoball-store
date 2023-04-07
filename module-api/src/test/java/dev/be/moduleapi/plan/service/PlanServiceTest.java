1be9d2e699b4b5b8b191c7193e3dda7e0b5ec0d7
mrcocoball <gunmaru93@gmail.com>
2023-04-07 16:27:32 +0900

    #111 - 플랜 API 단일 / 통합 테스트 수정
    * 플랜 수정 / 종료 테스트 케이스 추가
    * 플랜 단건 조회 실패 케이스 추가

--- module-api/src/test/java/dev/be/moduleapi/plan/service/PlanServiceTest.java
+++ module-api/src/test/java/dev/be/moduleapi/plan/service/PlanServiceTest.java
@@ -1,12 +1,14 @@
 package dev.be.moduleapi.plan.service;
 
 import dev.be.fixture.Fixture;
+import dev.be.moduleapi.advice.exception.PlanNotFoundApiException;
 import dev.be.moduleapi.plan.dto.PlanRequestDto;
 import dev.be.modulecore.domain.plan.Plan;
 import dev.be.modulecore.domain.user.User;
 import dev.be.modulecore.repositories.plan.DetailPlanRepository;
 import dev.be.modulecore.repositories.plan.PlanRepository;
 import dev.be.modulecore.repositories.user.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
@@ -56,16 +60,37 @@ class PlanServiceTest {
 
     }
 
-    @Disabled("단일 테스트로 돌리기 어려울 것 같다...")
     @DisplayName("UPDATE - 플랜 수정")
     @Test
     public void 플랜_수정_성공() {
 
         // Given
+        Plan plan = Fixture.plan();
+        PlanRequestDto updateRequestDto = Fixture.planUpdateRequestDto();
+        given(planRepository.findById(updateRequestDto.getId())).willReturn(Optional.of(plan));
 
         // When
+        sut.updatePlan(updateRequestDto);
 
         // Then
+        assertThat(plan).hasFieldOrPropertyWithValue("title", updateRequestDto.getTitle());
+        then(planRepository.findById(updateRequestDto.getId()));
+
+    }
+
+    @DisplayName("UPDATE - 플랜 수정 - 실패(존재하지 않는 플랜)")
+    @Test
+    public void 플랜_수정_실패() {
+
+        // Given
+        PlanRequestDto updateRequestDto = Fixture.planUpdateRequestDto();
+        given(planRepository.findById(updateRequestDto.getId())).willReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(PlanNotFoundApiException.class, () -> {
+            sut.updatePlan(updateRequestDto);
+        });
+        then(planRepository.findById(updateRequestDto.getId()));
 
     }
 
@@ -85,16 +110,37 @@ class PlanServiceTest {
 
     }
 
-    @Disabled("단일 테스트로 돌리기 어려울 것 같다...")
     @DisplayName("UPDATE - 플랜 종료 처리")
     @Test
     public void 플랜_종료_성공() {
 
         // Given
+        Plan plan = Fixture.plan();
+        Long id = plan.getId();
+        given(planRepository.findById(id)).willReturn(Optional.of(plan));
 
         // When
+        sut.finishPlan(id);
 
         // Then
+        assertThat(plan).hasFieldOrPropertyWithValue("finished", true);
+        then(planRepository.findById(id));
+
+    }
+
+    @DisplayName("UPDATE - 플랜 종료 처리 - 실패(존재하지 않는 플랜)")
+    @Test
+    public void 플랜_종료_실패() {
+
+        // Given
+        Long id = 1L;
+        given(planRepository.findById(id)).willReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(PlanNotFoundApiException.class, () -> {
+            sut.finishPlan(id);
+        });
+        then(planRepository.findById(id));
 
     }
 
