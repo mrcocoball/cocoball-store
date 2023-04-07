334ae746fad9872c9568b391d5e1a988a9272179
mrcocoball <gunmaru93@gmail.com>
2023-04-07 16:17:57 +0900

    #111 - 리뷰 API 단일 / 통합 테스트 수정
    * 리뷰 수정 관련 테스트 케이스 추가
    * 리뷰 단건 조회 실패 케이스 추가

--- module-api/src/test/java/dev/be/moduleapi/review/service/ReviewServiceTest.java
+++ module-api/src/test/java/dev/be/moduleapi/review/service/ReviewServiceTest.java
@@ -1,6 +1,8 @@
 package dev.be.moduleapi.review.service;
 
 import dev.be.fixture.Fixture;
+import dev.be.moduleapi.advice.exception.PlaceNotFoundApiException;
+import dev.be.moduleapi.advice.exception.ReviewNotFoundApiException;
 import dev.be.moduleapi.review.dto.ReviewRequestDto;
 import dev.be.modulecore.domain.place.Place;
 import dev.be.modulecore.domain.review.Review;
@@ -8,6 +10,7 @@ import dev.be.modulecore.domain.user.User;
 import dev.be.modulecore.repositories.place.PlaceRepository;
 import dev.be.modulecore.repositories.review.ReviewRepository;
 import dev.be.modulecore.repositories.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +20,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
@@ -56,6 +61,60 @@ class ReviewServiceTest {
         then(userRepository).should().findByNickname(dto.getNickname());
         then(placeRepository).should().findByPlaceId(dto.getPlaceId());
         then(reviewRepository).should().save(any(Review.class));
+
+    }
+
+    @DisplayName("CREATE - 리뷰 저장 - 실패(존재하지 않는 장소)")
+    @Test
+    public void 리뷰_저장_실패() {
+
+        // Given
+        User user = Fixture.user();
+        ReviewRequestDto dto = Fixture.reviewRequestDto();
+        given(userRepository.findByNickname(dto.getNickname())).willReturn(Optional.of(user));
+        given(placeRepository.findByPlaceId(dto.getPlaceId())).willReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(PlaceNotFoundApiException.class, () -> {
+            sut.saveReview(dto);
+        });
+        then(userRepository).should().findByNickname(dto.getNickname());
+        then(placeRepository).should().findByPlaceId(dto.getPlaceId());
+
+    }
+
+    @DisplayName("UPDATE - 리뷰 수정")
+    @Test
+    public void 리뷰_수정_성공() {
+
+        // Given
+        Review review = Fixture.review();
+        ReviewRequestDto updateRequestDto = Fixture.reviewUpdateRequestDto();
+        given(reviewRepository.findById(updateRequestDto.getId())).willReturn(Optional.of(review));
+
+        // When
+        sut.updateReview(updateRequestDto);
+
+        // Then
+        assertThat(review).hasFieldOrPropertyWithValue("title", updateRequestDto.getTitle());
+        assertThat(review).hasFieldOrPropertyWithValue("description", updateRequestDto.getDescription());
+        then(reviewRepository).should().findById(updateRequestDto.getId());
+
+    }
+
+    @DisplayName("UPDATE - 리뷰 수정 - 실패(존재하지 않는 리뷰)")
+    @Test
+    public void 리뷰_수정_실패() {
+
+        // Given
+        ReviewRequestDto updateRequestDto = Fixture.reviewUpdateRequestDto();
+        given(reviewRepository.findById(updateRequestDto.getId())).willReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(ReviewNotFoundApiException.class, () -> {
+            sut.updateReview(updateRequestDto);
+        });
+        then(reviewRepository).should().findById(updateRequestDto.getId());
 
     }
 
