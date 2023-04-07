b07f9d07ffea6c7e2c8bcb711f98997644a3a4d6
mrcocoball <gunmaru93@gmail.com>
2023-04-07 16:16:19 +0900

    #111 - 북마크 API 단일 / 통합 테스트 수정
    * 단건 조회 실패 케이스 추가

--- module-api/src/test/java/dev/be/moduleapi/bookmark/BookmarkServiceTest.java
+++ module-api/src/test/java/dev/be/moduleapi/bookmark/BookmarkServiceTest.java
@@ -1,6 +1,7 @@
 package dev.be.moduleapi.bookmark;
 
 import dev.be.fixture.Fixture;
+import dev.be.moduleapi.advice.exception.PlaceNotFoundApiException;
 import dev.be.moduleapi.bookmark.service.BookmarkService;
 import dev.be.modulecore.domain.bookmark.Bookmark;
 import dev.be.modulecore.domain.place.Place;
@@ -8,6 +9,7 @@ import dev.be.modulecore.domain.user.User;
 import dev.be.modulecore.repositories.bookmark.BookmarkRepository;
 import dev.be.modulecore.repositories.place.PlaceRepository;
 import dev.be.modulecore.repositories.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
@@ -53,6 +56,26 @@ class BookmarkServiceTest {
         then(userRepository).should().findByNickname(nickname);
         then(placeRepository).should().findByPlaceId(kpid);
         then(bookmarkRepository).should().save(any(Bookmark.class));
+
+    }
+
+    @DisplayName("CREATE - 북마크 저장 - 실패(존재하지 않는 장소)")
+    @Test
+    public void 북마크_저장_실패() {
+
+        // Given
+        String nickname = "test";
+        String kpid = "test";
+        User user = Fixture.user();
+        given(userRepository.findByNickname(nickname)).willReturn(Optional.of(user));
+        given(placeRepository.findByPlaceId(kpid)).willReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(PlaceNotFoundApiException.class, () -> {
+           sut.saveBookmark(nickname, kpid);
+        });
+        then(userRepository).should().findByNickname(nickname);
+        then(placeRepository).should().findByPlaceId(kpid);
 
     }
 
