6bb3daf8c4a33704964b4aa71c9318e9a8ceab21
mrcocoball <gunmaru93@gmail.com>
2023-04-07 16:15:48 +0900

    #111 - 미사용 Fixture 삭제

--- module-api/src/test/java/dev/be/fixture/Fixture.java
+++ module-api/src/test/java/dev/be/fixture/Fixture.java
@@ -7,7 +7,6 @@ import dev.be.moduleapi.review.dto.ReviewRequestDto;
 import dev.be.modulecore.domain.bookmark.Bookmark;
 import dev.be.modulecore.domain.place.Category;
 import dev.be.modulecore.domain.place.Place;
-import dev.be.modulecore.domain.plan.DetailPlan;
 import dev.be.modulecore.domain.plan.Plan;
 import dev.be.modulecore.domain.review.Review;
 import dev.be.modulecore.domain.user.User;
@@ -20,12 +19,10 @@ public class Fixture {
      * 엔티티
      */
 
-    // 테스트용 카테고리 (고정)
     public static Category category() {
         return Category.of("CE7", "카페");
     }
 
-    // 테스트용 유저 (고정)
     public static User user() {
         return User.builder()
                 .uid(1L)
@@ -37,7 +34,6 @@ public class Fixture {
                 .build();
     }
 
-    // 테스트용 장소 (고정)
     public static Place place() {
         return Place.of(
                 category(),
@@ -56,27 +52,24 @@ public class Fixture {
         );
     }
 
-    // 테스트용 장소 (동적)
-    public static Place place(String placeName, String placeId, String placeUrl, String addressName, String roadAddressName,
-                              String region1DepthName, String region2DepthName, String region3DepthName,
-                              double longitude, double latitude, Long reviewScore, Long reviewCount) {
+    public static Place newPlace() {
         return Place.of(
                 category(),
-                placeName,
-                placeId,
-                placeUrl,
-                addressName,
-                roadAddressName,
-                region1DepthName,
-                region2DepthName,
-                region3DepthName,
-                longitude,
-                latitude,
-                reviewScore,
-                reviewCount);
+                "testplace2",
+                "2",
+                "testurl2",
+                "서울 관악구 봉천동",
+                "서울 관악구 봉천동",
+                "서울",
+                "관악구",
+                "봉천동",
+                126.94,
+                37.47,
+                0L,
+                0L
+        );
     }
 
-    // 테스트용 리뷰 (고정)
     public static Review review() {
         return Review.of(
                 user(),
@@ -88,19 +81,6 @@ public class Fixture {
         );
     }
 
-    // 테스트용 리뷰 (업데이트 리뷰)
-    public static Review updateReview(String title, String description, Long reviewScore) {
-        return Review.of(
-                user(),
-                place(),
-                "1",
-                title,
-                description,
-                reviewScore
-        );
-    }
-
-    // 테스트용 북마크 (고정)
     public static Bookmark bookmark() {
         return Bookmark.of(
                 user(),
@@ -109,15 +89,6 @@ public class Fixture {
         );
     }
 
-    // 테스트용 북마크 (동적)
-    public static Bookmark bookmark(User user, Place place, String kpid) {
-        return Bookmark.of(
-                user,
-                place,
-                kpid);
-    }
-
-    // 테스트용 플랜 (고정)
     public static Plan plan() {
         return Plan.of(
                 user(),
@@ -127,41 +98,11 @@ public class Fixture {
         );
     }
 
-    // 테스트용 플랜 (업데이트 플랜)
-    public static Plan updatePlan(String title, boolean finished, String comment) {
-        return Plan.of(
-                user(),
-                title,
-                finished,
-                comment
-        );
-    }
-
-    // 테스트용 세부 플랜 (고정)
-    public static DetailPlan detailPlan() {
-        return DetailPlan.of(
-                plan(),
-                place(),
-                "1",
-                1
-        );
-    }
-
-    // 테스트용 세부 플랜 (업데이트 세부 플랜)
-    public static DetailPlan updateDetailPlan(Place place, String kpid, int ord) {
-        return DetailPlan.of(
-                plan(),
-                place,
-                kpid,
-                ord
-        );
-    }
 
     /**
      * DTO
      */
 
-    // 카카오 API Response 내의 DocumentDto (고정)
     public static DocumentDto documentDto() {
         return DocumentDto.builder()
                 .categoryGroupId("CE7")
@@ -178,11 +119,6 @@ public class Fixture {
                 .build();
     }
 
-    // 테스트용 장소 (고정)
-
-    // 테스트용 장소 (동적)
-
-    // 테스트용 리뷰 (고정)
     public static ReviewRequestDto reviewRequestDto() {
         return ReviewRequestDto.of(
                 1L,
@@ -209,13 +145,6 @@ public class Fixture {
         );
     }
 
-    // 테스트용 리뷰 (동적)
-
-    // 테스트용 북마크 (고정)
-
-    // 테스트용 북마크 (동적)
-
-    // 테스트용 플랜 (고정)
     public static PlanRequestDto planRequestDto() {
         return PlanRequestDto.of(
                 1L,
@@ -232,9 +161,6 @@ public class Fixture {
         );
     }
 
-    // 테스트용 플랜 (동적)
-
-    // 테스트용 세부 플랜 (고정)
     public static DetailPlanRequestDto detailPlanRequestDto() {
         return DetailPlanRequestDto.of(
                 1L,
@@ -252,7 +178,5 @@ public class Fixture {
                 "2"
         );
     }
-
-    // 테스트용 세부 플랜 (동적)
 
 }
