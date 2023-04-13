3d24836ae242a5e236da40c6da4ed6de5c137f86
mrcocoball <gunmaru93@gmail.com>
2023-04-13 09:34:52 +0900

    #121 - Swagger-UI 설정 변경
    * 기존에는 Parameter 어노테이션으로 X-AUTH-TOKEN을 전달받아 인증을 받았으나 SecurityScheme을 추가하여 전역적인 JWT 인증을 할 수 있도록 변경하였음

--- module-api/src/main/java/dev/be/moduleapi/config/OpenApiConfig.java
+++ module-api/src/main/java/dev/be/moduleapi/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package dev.be.moduleapi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,18 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI config() {
+
+        String jwtSchemeName = "jwtAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("Authorization"));
+
         return new OpenAPI()
                 .info(new Info().title("Date-planner API 서버")
                         .description("API 명세서와 테스트를 위한 API를 제공하고 있습니다")
@@ -17,7 +32,9 @@ public class OpenApiConfig {
                         .contact(new io.swagger.v3.oas.models.info.Contact()
                                 .name("더블코코볼 mrcocoball")
                                 .email("gunmaru93@gmai.com")
-                                .url("https://github.com/mrcocoball/date-planner")));
+                                .url("https://github.com/mrcocoball/date-planner")))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
 
