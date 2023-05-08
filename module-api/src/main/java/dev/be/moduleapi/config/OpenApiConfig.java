7d866d431c7149c429a6ace54f8802b86a1af53e
mrcocoball <gunmaru93@gmail.com>
2023-05-08 16:40:03 +0900

    #171 - Swagger-UI 설정 수정
    * 문서 제목명 변경
    * application.yml 에서 별도 프로필 추가, 운영 환경에서는 문서 노출 X

--- module-api/src/main/java/dev/be/moduleapi/config/OpenApiConfig.java
+++ module-api/src/main/java/dev/be/moduleapi/config/OpenApiConfig.java
@@ -26,27 +26,15 @@ public class OpenApiConfig {
                         .bearerFormat("Authorization"));
 
         return new OpenAPI()
-                .info(new Info().title("Date-planner API 서버")
+                .info(new Info().title("Pla&Plan API 서버 (로컬, 개발용)")
                         .description("API 명세서와 테스트를 위한 API를 제공하고 있습니다")
                         .version("v1")
                         .contact(new io.swagger.v3.oas.models.info.Contact()
                                 .name("더블코코볼 mrcocoball")
-                                .email("gunmaru93@gmai.com")
+                                .email("gunmaru93@gmail.com")
                                 .url("https://github.com/mrcocoball/date-planner")))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
 
-
-    /*
-
-    // 그룹 설정된 API들만 문서에 노출시킬 수 있게 한다
-    @Bean
-    public GroupedOpenApi sampleGroupOpenApi() {
-        String[] paths = {"/diary/**"};
-
-        return GroupedOpenApi.builder().group("샘플 API").pathsToMatch(paths).build();
-    }
-
-     */
 }
